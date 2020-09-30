464c5d4b0b0512ae2f321361621ed85fcebbf2fe
Oubayda Khayata <oubaydakhayata@gmail.com>
2020-09-30 13:03:27 +0300

    Add json helper function

---
+++ app/Http/Helpers/response.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!function_exists('json')) {
+    function json($status = '', $message = '', $data = '', $code = 200)
+    {
+
+        $response['status'] = $status;
+        $response['message'] = $message;
+        $response['data'] = $data;
+
+        return response()->json($response, $code);
+    }
+}
