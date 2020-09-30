6cad73c80f6a8d691b6457e81e35f29cca9d74dc
Oubayda Khayata <oubaydakhayata@gmail.com>
2020-09-30 13:41:22 +0300

    Add Signup request

---
+++ app/Http/Requests/SignupRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class SignupRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6'
+        ];
+    }
+
+    /**
+    * Handle a failed validation attempt.
+    *
+    * @param  \Illuminate\Contracts\Validation\Validator  $validator
+    * @return void
+    *
+    * @throws \Illuminate\Validation\ValidationException
+    */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(json('error', $validator->errors()->all(), '', 400));
+    }
+}
