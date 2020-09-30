822a91768f1234a4102e1e666ed693c959c4b836
Oubayda Khayata <oubaydakhayata@gmail.com>
2020-09-30 13:42:11 +0300

    Add Trending request

---
+++ app/Http/Requests/TrendingRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class TrendingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'limit' => 'filled|numeric',
+            'offset' => 'filled|numeric'
+        ];
+    }
+
+    /**
+    * Handle a failed validation attempt.
+    *
+    * @param  \Illuminate\Contracts\Validation\Validator  $validator
+    * @return void
+    *
+    * @throws \Illuminate\Validation\ValidationException
+    */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(json('error', $validator->errors()->all(), '', 400));
+    }
+}
